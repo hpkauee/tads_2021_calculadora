9863f582056b11a2ea5d3f6e7ce1af39c15a86b3
hpkauee <kauepaula82@gmail.com>
2021-11-05 21:29:14 -0300

    First commit

---
+++ R/diminui.R
@@ -0,0 +1,17 @@
+#' Função diminui
+#'
+#'  Esta é uma função simples para diminuir dois números
+#'
+#'  @param a Primeiro número
+#'  @param b Segundo número
+#'
+#'   @examples
+#'   diminui(3,2)
+#'   diminui(5,3)
+#'
+#'
+#' @export
+diminui <- function(a,b)
+{
+  a - b
+}
